59c1780aa2f2c34040ddfb7267ca74a577f6088f
blueskyokboy <blueskyokboy@gmail.com>
2021-11-07 23:33:04 +0800

    demo github

---
+++ src/demo2.java
@@ -0,0 +1,6 @@
+public class demo2 {
+    public static void main(String[] args){
+        var s="blueskyo demo ok";
+        System.out.println(s);
+    }
+}
---
+++ src/test.java
@@ -0,0 +1,12 @@
+public class test {
+    public static void main(String[] arg)
+    {
+        int i=6;
+        String m="9";
+        System.out.println("ok"+i+m);
+    }
+    public  void test2(){
+        var ok=9;
+        var ddd=3;
+    }
+}
