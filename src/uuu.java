c0ede933e3eb52e3c1ebe0bfcd526e3092598c11
blueskyokboy <blueskyokboy@gmail.com>
2021-11-13 21:36:22 +0800

    demo

---
+++ src/uuu.java
@@ -0,0 +1,18 @@
+public class uuu {
+    int i=99;
+    int y=9888;
+
+    public static void main (String [] eee){
+  var o="88asdlkjfffffffffffffffffffffffffffffffffffaaaaaaaaajalsdj foaisdjf poasidjfoai sdjfoiasjd fopasijdf oasijdflkasjdfl" +
+          "asdfiaposdij fpoaisdjfoijasxcvzxcvxcoidjfaisod" +
+          "a" +
+          "sdfjaspodijf a;osdijfa sjidf klasd" +
+          "f" +
+          " asdf" +
+          "asdf" +
+          "sad" +
+          "f" +
+          "asd" ;
+  System.out.println(o);
+    }
+}
